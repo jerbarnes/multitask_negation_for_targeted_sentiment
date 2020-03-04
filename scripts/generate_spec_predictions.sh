632c1929e25d28717774488e84781263b301d56f
Andrew Moore <andrew.p.moore94@gmail.com>
2020-03-04 21:17:29 +0000

    MTL SPEC predictions for upos, dr, lextag

--- scripts/generate_spec_predictions.sh
+++ scripts/generate_spec_predictions.sh
@@ -10,4 +10,9 @@ python ./scripts/generate.py ./data/models/en/mtl/conan_doyle/laptop ./data/main
 python ./scripts/generate.py ./data/models/en/mtl/sfu/laptop ./data/main_task/en/laptop/dev_spec.conll ./data/results/en/mtl/sfu/laptop --cuda
 # MTL SFU SPEC
 python ./scripts/generate.py ./data/models/en/mtl/sfu_spec/laptop ./data/main_task/en/laptop/dev_spec.conll ./data/results/en/mtl/sfu_spec/laptop --cuda
-
+# MTL UPOS
+python ./scripts/generate.py ./data/models/en/mtl/u_pos/laptop ./data/main_task/en/laptop/dev_spec.conll ./data/results/en/mtl/u_pos/laptop --cuda
+# MTL DR
+python ./scripts/generate.py ./data/models/en/mtl/dr/laptop ./data/main_task/en/laptop/dev_spec.conll ./data/results/en/mtl/dr/laptop --cuda
+# MTL LEXTAG
+python ./scripts/generate.py ./data/models/en/mtl/lextag/laptop ./data/main_task/en/laptop/dev_spec.conll ./data/results/en/mtl/lextag/laptop --cuda
